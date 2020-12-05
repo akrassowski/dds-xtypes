9696a4490cbe571d4d33e93e9014d2151069ae9f
Andy Krassowski <akrassowski@gmail.com>
2020-12-04 20:15:03 -0500

    Tweak makefile for Ubuntu 16.04

--- devpackageinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install make gcc g++ -y
-sudo apt-get install gcc-multilib g++-multilib -y
-
-
-
--- fixup_extensibility_idl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# change TwinOaks 
-#    Extensibility( blah) 
-# to OMG's / RTI's 
-#   Extensibility blah
-# The passed file is modified in-place
-sed --in-place 's:Extensibility[(]\(.*\)[)]:Extensibility \1:' $1
